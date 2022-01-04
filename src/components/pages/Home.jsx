6a430fb59511d7bfa76e649d42a05486451c89b0
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-01-05 00:26:32 +0530

    feat: use the button component and correctly position it

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,16 @@
+import Button from "../common/Button";
 function Home() {
   return (
     <div className="grid grid-cols-12 h-screen">
       <div className="col-span-6">
       </div>
-      <div className="bg-black col-span-6">
+      <div className="bg-black col-span-6 relative">
+        <Button
+          variant='dark'
+          text='Connect Wallet'
+          onClick={() => console.log('connecting wallet...') }
+          className="absolute top-10 right-10"
+        />
       </div>
     </div>
   );
