40fe0a2d72fe161b2b8b8352939b9d34d5c96f64
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-01-04 23:45:25 +0530

    feat: break the App.js into 2 equal parts

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="grid text-3xl font-bold underline">
-        Hello World!
+      <div className="grid grid-cols-12">
+        <div className="col-span-6">
+        </div>
+        <div className="col-span-6">
+            Wallet Section
+        </div>
       </div>
     </div>
   );
