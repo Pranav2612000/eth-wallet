82e70a3011502590eac1ee1a1092a753e726cdaa
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-01-05 23:37:08 +0530

    feat: build the amount selector for invest page

--- src/components/pages/Invest.jsx
+++ src/components/pages/Invest.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import Button from "../common/Button";
 import Logo from "../common/Logo";
 import PurpleCircle from "../common/PurpleCircle";
+import Slider from "../common/Slider";
+import Img from "../common/Img";
+import ethereumLogo from '../../assets/images/icons/ethereum-light.svg'
 
 function Invest() {
+  const [total, setTotal] = useState(0); 
+  const [lending, setLending] = useState(0); 
+  const [staking, setStaking] = useState(0); 
+  const [swaping, setSwaping] = useState(0); 
+
   return (
     <div className="grid grid-cols-12 h-screen">
       <div className="col-span-12 lg:col-span-6 string-bg pt-10 px-16">
@@ -23,8 +32,81 @@ function Invest() {
           onClick={() => console.log('connecting wallet...') }
           className="absolute top-10 right-10"
         />
-        <div className="h-2/5 w-3/5 text-white glass-effect rounded-[30px]">
-          <p className="text-center text-sm mt-8">What percentage of wallet money you want to auto invest?</p>
+        <div className="w-3/5 text-white glass-effect rounded-[30px] px-14 py-8">
+          <p className="text-bold text-sm">What percentage of wallet money you want to auto invest?</p>
+          <div className="mt-10">
+            <Slider setValue={setTotal}/>
+            <p className="text-center mt-3">{total}%</p>
+          </div>
+          <div>
+            <div className="flex justify-between mt-8">
+              <p> Lending [ {lending}% ] </p>
+              <div className="flex justify-center items-center px-8 bg-purple-gradient rounded-sm">
+                <Img  
+                  srcSet= {
+                    [
+                      {
+                        'srcSet': [
+                          ethereumLogo 
+                        ]
+                      }
+                    ]
+                  }
+                  isFromAssets={false}
+                />
+                <span className="ml-2">60</span>
+              </div>
+            </div>
+            <div className="mt-5">
+              <Slider setValue={setLending}/>
+            </div>
+          </div>
+          <div>
+            <div className="flex justify-between mt-8">
+              <p> Staking [ {staking}% ] </p>
+              <div className="flex justify-center items-center px-8 bg-purple-gradient rounded-sm">
+                <Img  
+                  srcSet= {
+                    [
+                      {
+                        'srcSet': [
+                          ethereumLogo 
+                        ]
+                      }
+                    ]
+                  }
+                  isFromAssets={false}
+                />
+                <span className="ml-2">60</span>
+              </div>
+            </div>
+            <div className="mt-5">
+              <Slider setValue={setStaking}/>
+            </div>
+          </div>
+          <div className="pb-5">
+            <div className="flex justify-between mt-8">
+              <p> Swaping [ {swaping}% ] </p>
+              <div className="flex justify-center items-center px-8 bg-purple-gradient rounded-sm">
+                <Img  
+                  srcSet= {
+                    [
+                      {
+                        'srcSet': [
+                          ethereumLogo 
+                        ]
+                      }
+                    ]
+                  }
+                  isFromAssets={false}
+                />
+                <span className="ml-2">60</span>
+              </div>
+            </div>
+            <div className="mt-5">
+              <Slider setValue={setSwaping}/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'purple-gradient': 'linear-gradient(125.15deg, rgba(117, 0, 212, 0.5) 11%, rgba(209, 30, 104, 0.5) 97.55%)',
+      }
+    },
   },
   plugins: [],
 }
