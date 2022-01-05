c9f1d9d2ed1c8670fccc8e9b77870cd01c28d444
Pranav2612000 <pranav2000joglekar@gmail.com>
2022-01-05 21:03:10 +0530

    feat: build the left part of the invest page

--- src/components/pages/Invest.jsx
+++ src/components/pages/Invest.jsx
@@ -7,7 +7,12 @@ function Invest() {
     <div className="grid grid-cols-12 h-screen">
       <div className="col-span-12 lg:col-span-6 string-bg pt-10 px-16">
         <Logo/>
-        <PurpleCircle className={"w-[130px] h-[130px] top-36 left-4"}/>
+        <PurpleCircle className={"w-[148px] h-[148px] top-[65%] left-[-60px]"}/>
+        <h2 className="mt-[30vh] text-[3.25rem] leading-[68px]">
+          Choose your <br/>
+          investing <br/>
+          options wisely.
+        </h2>
       </div>
       <div className="bg-black col-span-12 lg:col-span-6 relative flex items-center justify-center">
         <PurpleCircle className={"w-[111px] h-[111px] top-[calc(30%-55px)] left-[calc(20%-55px)]"}/>
